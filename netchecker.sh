c1f4e1485e659ba51d93f4f8afe2d11b49ba8aaa
killkong <91668536+killkong@users.noreply.github.com>
2022-01-31 23:17:33 +0300

    Add files via upload
    init

---
+++ netchecker.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+# settings
+KEEP_FOR=60     # seconds
+DELAY=10
+CYCLE=0
+
+while true; do
+
+    DATE=`date +%h-%d\(%H:%M:%S\)`    
+
+    if [[ "$CYCLE" -ge 1900 ]]; then
+            CYCLE=0
+    fi
+
+    PING=$(echo "$(ping -c3 google.com| grep "received")")
+    RECEIVED=$(echo $PING | awk '{printf $1}')
+
+    if [[ "${RECEIVED}" = "" ]]; then
+        PING=$(echo "$(ping -c3 ya.ru | grep "received")")
+        RECEIVED=$(echo $PING | awk '{printf $1}')
+    fi 
+
+    if [[ "${RECEIVED}" -ge 3 ]]; then
+        CYCLE=0
+        sleep $DELAY
+    else 
+        sleep $DELAY
+        CYCLE="$(($CYCLE + $DELAY))"
+        if [[ "$CYCLE" -ge "$KEEP_FOR" ]]; then
+            printf "REBOOT \n"
+            sudo reboot
+        fi    
+    fi    
+
+    let "MIN = $CYCLE / 30"
+    printf "$DATE: received: $RECEIVED, net delay for $MIN min \n" 
+
+done
